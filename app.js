10404c1caac0d362ef19f5804f7c5034745660ba
Antoine Hébert <ahebert@et.esiea.fr>
2017-04-06 10:45:44 +0200

    Importation de la structure du projet

---
+++ app.js
@@ -0,0 +1,18 @@
+var db = require('./db')
+
+app.use('/comments', require('./controllers/comments'))
+app.use('/users', require('./controllers/users'))
+
+// Connect to MySQL on start
+db.connect(db.MODE_PRODUCTION, function(err) {
+  if (err) {
+    console.log('Unable to connect to MySQL.')
+    process.exit(1)
+  } else {
+    app.listen(3000, function() {
+      console.log('Listening on port 3000...')
+    })
+  }
+})
+
+
