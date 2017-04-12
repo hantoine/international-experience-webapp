0d5750f6facf629dc6d98ea774bf60b02ad02dec
Antoine Hébert <ahebert@et.esiea.fr>
2017-04-12 14:40:27 +0200

    Instalation des lib nécessaires

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-var db = require('./db')
+var config = require('./config');
+var express = require('express');
+var morgan  = require('morgan'); // Utilisé pour les logs
+var session = require('cookie-session');
+var bodyParser = require('body-parser'); // Permettra de récupérer les données issues d'un formulaire
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var db = require('./db');
+var app = express();
 
-app.use('/comments', require('./controllers/comments'))
-app.use('/users', require('./controllers/users'))
+// Midlewares
+app.use(express.static(__dirname + '/public')); // Fichiers statique (css, images, etc.)
+app.use(session({secret: 'xDqgDkEIiRX9CVdHS3UhyZYtDD4ovK+W2VNIbPBWMJ0vwsSznNc/sA=='}))
+app.use(morgan('combined')); // Logging
+
+app.use('/form', require('./controllers/form'))
+app.use(function(req, res, next){
+	res.setHeader('Content-type', 'text/plain');
+	res.send(404, 'Page introuvable !');
+});
 
 // Connect to MySQL on start
-db.connect(db.MODE_PRODUCTION, function(err) {
+db.connect(config.databaseMode, function(err) {
   if (err) {
-    console.log('Unable to connect to MySQL.')
-    process.exit(1)
+    console.log('Unable to connect to MySQL.');
+    process.exit(1);
   } else {
-    app.listen(3000, function() {
-      console.log('Listening on port 3000...')
+    app.listen(config.listeningPort, function() {
+      console.log('Listening on port ' + config.listeningPort + '...');
     })
   }
 })
---
+++ config.js
@@ -0,0 +1,7 @@
+MODE_TEST = 'mode_test'
+MODE_PRODUCTION = 'mode_production'
+
+module.exports = {
+	listeningPort: 8080,
+	databaseMode: MODE_TEST
+}
