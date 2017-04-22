1bd3170a6e009ef93e631ad179516156614c6d83
Antoine Hébert <ahebert@et.esiea.fr>
2017-04-22 11:39:07 +0200

    Ajout section admin vide (/login/logout

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-});
-
-
 router.get('/login', function(req, res, next) {
 	if(req.session.logged) {
 		res.redirect('/admin');
 	} else {
 		res.render('admin/login.ejs', {});
 	}
+});
+
+router.post('/login', function(req, res, next) {
+	if(req.body.password == "troisdeuxun") {
+		req.session.logged = true;
+		res.redirect('/admin');
+	}
+});
+router.get('/logout', function(req, res, next) {
+	req.session.logged = false;
+	res.redirect('/');
+});
+
+router.get('/', function(req, res, next) {
+	if(! req.session.logged) {
+		res.redirect('/admin/login');
+		return;
+	}
+	res.send('Not implemented yet');
 });
 
 module.exports = router;
