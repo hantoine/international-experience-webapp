937af00167beef2ac3ae3af47f32ecbb33fe12c0
Amelie <root@localhost.localdomain>
2017-04-22 13:35:34 +0200

    ajout css + models experience

--- models/experience.js
+++ models/experience.js
@@ -24,3 +24,40 @@ exports.checkStudentHasAccess = function(studentid, expid, hasAccessFct, hasNotA
 		}
 	});
 };
+
+
+exports.getListWithLocation = function(continentsid,contryid,cityid, universityid, done) {
+	query = 'SELECT e.id_experience, o.nom, v.nom, p.nom, c.nom, e.age, e.duree FROM experience e
+
+		JOIN organisation o ON e.id_organisation = o.id_organisation
+		JOIN ville v ON o.id_ville = v.id_ville
+		JOIN pays p ON v.id_pays = p.id_pays
+		JOIN continent c ON p.id_continent = c.id_continent';
+
+	if (continentid || contryid || cityid || universityid){
+		query += ' WHERE ';
+
+		if (universityid)
+		{
+			query += 'o.id_organisation = ' + db.escape(universityid)
+		}
+		else if(cityid)
+		{
+			query += 'v.id_ville = ' + db.escape(cityid)
+		}
+		else if (contryid)
+		{
+			query += 'p.id_pays = ' + db.escape(contryid)
+		}
+		else if (continentid)
+		{
+			query += 'c.id_continent = ' + db.escape(continentid)
+		}
+	}
+	console.log(query);
+	db.get().query(query, function(err,rows) {
+		if(err) return done(err);
+		done(null, rows);
+	});
+}
+
